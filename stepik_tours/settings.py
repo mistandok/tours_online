2ba4ec143aee5a9fcacd29ceeb3a5dbc47546d28
artikov.ak <arti-anton@yandex.ru>
2022-04-01 15:01:09 +0300

    add staticfiles dirs

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
