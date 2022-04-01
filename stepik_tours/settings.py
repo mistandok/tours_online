9e5b131661220443c01406abe8e0b171a5a4095d
artikov.ak <arti-anton@yandex.ru>
2022-04-01 14:22:11 +0300

    finaly staticfiles destination

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -118,10 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
